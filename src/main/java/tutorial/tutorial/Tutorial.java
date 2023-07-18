ab670d52f6b941f832dd8123770e39c6e11ab1c1
BGOPC <bb8933400@gmail.com>
2023-07-18 18:36:16 +0330

    Edited Maven config, basic hello

--- src/main/java/tutorial/tutorial/Tutorial.java
+++ src/main/java/tutorial/tutorial/Tutorial.java
@@ -1,17 +1,18 @@
 package tutorial.tutorial;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Tutorial extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getLogger().info("Hello World");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info("Goodbye World");
     }
 }
