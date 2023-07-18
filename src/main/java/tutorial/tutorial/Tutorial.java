bc3c1167ceb65b6ef85fffd7b6df911af96ac934
BGOPC <bb8933400@gmail.com>
2023-07-18 18:17:33 +0330

    Initial Commit

---
+++ src/main/java/tutorial/tutorial/Tutorial.java
@@ -0,0 +1,17 @@
+package tutorial.tutorial;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tutorial extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
